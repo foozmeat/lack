40c6a777047b3e336bc3d4cb93414162e98e3c67
James Moore <james@panic.com>
2017-04-23 10:11:39 -0700

    refactor the logwindow

---
+++ lack/logwindow.py
@@ -0,0 +1,122 @@
+import curses
+import math
+from datetime import datetime
+
+import os
+import pytz
+from sortedcontainers import SortedDict
+
+DOWN = 1
+UP = -1
+
+
+class LogWindow:
+    def __init__(self,
+                 height: int,
+                 width: int,
+                 top: int,
+                 left: int,
+                 datasource: SortedDict) -> None:
+
+        self.height = height
+        self.width = width
+        self.top = top
+        self.left = left
+        self.datasource = datasource
+        self.topline = 0
+        self.last_log_length = 0
+        self.log_length = 0
+
+        self.window = curses.newwin(self.height,
+                                    self.width,
+                                    self.top,
+                                    self.left)
+
+        self.window_y, self.window_x = self.window.getbegyx()
+        self.rows, self.cols = self.window.getmaxyx()
+
+        # self.window.scrollok(True)
+        self.window.idlok(1)
+
+        self.scrollbar_x = self.width - 1
+        self.line_length = self.width - 2
+        self._tz = os.getenv('SLACK_TZ', 'UTC')
+        self.window.refresh()
+
+    def log_up_down(self, increment):
+        scroll_max = self.log_length - self.height
+
+        if self.topline > 0 and increment == UP:
+            self.topline -= 1
+
+        elif self.topline < scroll_max and increment == DOWN:
+            self.topline += 1
+
+        self.draw()
+
+    def draw(self):
+        self.log_length = len(self.datasource)
+
+        if self.log_length != self.last_log_length and self.log_length > self.height:
+            self.topline = self.log_length - self.height
+            # bottom = self.log_length
+
+        bottom = self.topline + self.height
+
+        if bottom > self.log_length:
+            bottom = self.log_length
+
+        log_keys = self.datasource.iloc[self.topline:bottom]
+
+        tz = pytz.timezone(self._tz)
+
+        for (index, ts) in enumerate(log_keys):
+
+            posix_timestamp, _ = ts.split('.')
+            posix_timestamp = int(posix_timestamp)
+            utc_dt = datetime.fromtimestamp(posix_timestamp)
+            dt = utc_dt.astimezone(tz)
+            date = dt.strftime('%A %I:%M%p')
+
+            line = self.datasource[ts]
+            color, name, msg = line
+            self.window.attron(curses.color_pair(color))
+
+            line = f"{date} {name}: {msg}"
+
+            if len(line) > self.line_length:
+                line = line[0:self.line_length]
+
+            self.window.addstr(index, 0, line)
+            self.window.clrtoeol()
+            self.window.attroff(curses.color_pair(color))
+
+        self.last_log_length = self.log_length
+
+        self._draw_scrollbar()
+        # self.window.addstr(1, 5, f"{self.height} {self.width} {self.top} {self.left}")
+        self.window.refresh()
+
+    def _draw_scrollbar(self):
+        self.window.vline(1,
+                          self.scrollbar_x,
+                          ' ',
+                          self.height)
+
+        if self.log_length > self.height:
+            overflow = self.log_length - self.height
+
+            scrollbar_length = self.height * (float(self.height) / float(self.log_length))
+            scrollbar_length = max(scrollbar_length, 3)
+            scrollbar_length = int(math.floor(scrollbar_length))
+
+            scrollbar_steps = overflow / float(self.height - scrollbar_length)
+            scrollbar_y_float = (self.topline / scrollbar_steps)
+            scrollbar_y = int(round(1 + scrollbar_y_float, 0))
+
+            self.window.vline(scrollbar_y,
+                              self.scrollbar_x,
+                              curses.ACS_CKBOARD,
+                              scrollbar_length)
+
+            self.window.refresh()
--- lack/screen.py
+++ lack/screen.py
@@ -1,25 +1,18 @@
 import asyncio
 import curses
-import math
 import signal
 from curses import panel
 from curses.textpad import Textbox
-from datetime import datetime
 
 import os
-import pytz
 
 from .lackmanager import LackManager
-
-DOWN = 1
-UP = -1
+from .logwindow import UP, DOWN, LogWindow
 
 
 class LackScreen:
     rows = 0
     cols = 0
-    last_log_length = 0
-    log_length = 0
     msgpad = None
 
     def __init__(self, window):
@@ -38,7 +31,6 @@ class LackScreen:
         self.window_y, self.window_x = self.window.getbegyx()
 
         self.rows, self.cols = self.window.getmaxyx()
-        self.scrollbar_x = self.cols - 2
 
         curses.use_default_colors()
         for i in range(0, curses.COLORS):
@@ -46,16 +38,16 @@ class LackScreen:
 
         self.logwin_top = self.window_y + 1
         self.logwin_left = self.window_x + 2
-
         self.logwin_height = self.rows - 5
-        self.logwin_topline = 0
-        self.logwin_width = self.cols - 5
-        self.logwin = curses.newwin(self.logwin_height,
-                                    self.logwin_width,
-                                    self.logwin_top,
-                                    self.logwin_left)
-        self.logwin.scrollok(True)
-        self.logwin.idlok(1)
+        self.logwin_width = self.cols - 4
+
+        self.logwin = LogWindow(
+            self.logwin_height,
+            self.logwin_width,
+            self.logwin_top,
+            self.logwin_left,
+            self.lack_manager.loglines
+        )
 
         self.promptwin = curses.newwin(2,
                                        self.logwin_width,
@@ -73,8 +65,6 @@ class LackScreen:
                           curses.ACS_HLINE,
                           self.cols - 2)
 
-        self.log_length = len(self.lack_manager.loglines)
-
         self._tz = os.getenv('SLACK_TZ', 'UTC')
         self.hidden = False
 
@@ -84,78 +74,6 @@ class LackScreen:
     def resize_handler(self, signum, frame):
         # if we don't trap the window resize we'll just crash
         pass
-
-    def _log_up_down(self, increment):
-        scroll_max = self.log_length - self.logwin_height
-
-        if self.logwin_topline > 0 and increment == UP:
-            self.logwin_topline -= 1
-
-        elif self.logwin_topline < scroll_max and increment == DOWN:
-            self.logwin_topline += 1
-
-        self.draw()
-
-    def _draw_log(self):
-        self.log_length = len(self.lack_manager.loglines)
-
-        if self.log_length != self.last_log_length and self.log_length > self.logwin_height:
-            self.logwin_topline = self.log_length - self.logwin_height
-            self.bottom = self.log_length
-
-        self.bottom = self.logwin_topline + self.logwin_height
-
-        if self.bottom > self.log_length:
-            self.bottom = self.log_length
-
-        log_keys = self.lack_manager.loglines.iloc[self.logwin_topline:self.bottom]
-
-        tz = pytz.timezone(self._tz)
-
-        for (index, ts) in enumerate(log_keys):
-
-            posix_timestamp, _ = ts.split('.')
-            posix_timestamp = int(posix_timestamp)
-            utc_dt = datetime.fromtimestamp(posix_timestamp)
-            dt = utc_dt.astimezone(tz)
-            date = dt.strftime('%A %I:%M%p')
-
-            line = self.lack_manager.loglines[ts]
-            color, name, msg = line
-            self.logwin.attron(curses.color_pair(color))
-
-            if len(msg) > self.logwin_width:
-                msg = msg[0:self.logwin_width]
-
-            self.logwin.addstr(index, 0, "{} {}: {}".format(date, name, msg))
-            self.logwin.clrtoeol()
-            self.logwin.attroff(curses.color_pair(color))
-
-        self.logwin.refresh()
-
-    def _draw_scrollbar(self):
-        self.window.vline(1,
-                          self.scrollbar_x,
-                          ' ',
-                          self.logwin_height)
-
-        if self.log_length > self.logwin_height:
-            overflow = self.log_length - self.logwin_height
-
-            scrollbar_length = self.logwin_height * (float(self.logwin_height) / float(self.log_length))
-            scrollbar_length = max(scrollbar_length, 3)
-            scrollbar_length = int(math.floor(scrollbar_length))
-
-            scrollbar_steps = overflow / float(self.logwin_height - scrollbar_length)
-            scrollbar_y_float = (self.logwin_topline / scrollbar_steps)
-            scrollbar_y = int(round(1 + scrollbar_y_float, 0))
-
-            self.window.vline(scrollbar_y,
-                              self.scrollbar_x,
-                              curses.ACS_CKBOARD,
-                              scrollbar_length)
-
-            self.window.refresh()
 
     def _validator(self, ch):
 
@@ -168,10 +86,10 @@ class LackScreen:
             return curses.KEY_BACKSPACE
 
         elif ch == curses.KEY_UP:
-            self._log_up_down(UP)
+            self.logwin.log_up_down(UP)
 
         elif ch == curses.KEY_DOWN:
-            self._log_up_down(DOWN)
+            self.logwin.log_up_down(DOWN)
 
         return ch
 
@@ -244,11 +162,9 @@ class LackScreen:
         if not self.hidden:
             yield from asyncio.sleep(0.025)  # 24 fps
             self.window.refresh()
-            self._draw_log()
-            self._draw_scrollbar()
+            self.logwin.draw()
             self._prompt()
             self._draw_bottom()
-            self.last_log_length = self.log_length
         asyncio.async(self.draw())
 
 
